53f58fe0082f95fb2049d69fc26f67cbc497c324
github username <github email address>
2021-03-10 16:18:27 -0800

    added cors

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Wine.create([
+    {name: "La Crema", varietal: "Chardonnay", vintage: "2015"},
+    {name: "Duckhorn", varietal: "Cabernet", vintage: "2016"},
+    {name: "Rombaueur", varietal: "Merlot", vintage: "2018"},
+    {name: "Silver Oak", varietal: "Cabernet", vintage: "2015"},
+])
